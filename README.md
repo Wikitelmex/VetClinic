e110babe164d29647dc5e4fcc557f139daf133cf
Alex Castillo <wikitelmex@gmail.com>
2021-12-06 12:44:12 -0700

    Summary
    - add schema new database called vet_clinic
    - add schema new table called animals
    - add 4 new records to table animals
    - add queries asked in the exercise

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ This repository includes files with plain SQL that can be used to recreate a dat
 - Use [data.sql](./data.sql) to populate tables with sample data.
 - Check [queries.sql](./queries.sql) for examples of queries that can be run on a newly created database. **Important note: this file might include queries that make changes in the database (e.g., remove records). Use them responsibly!**
 
+## Screenshots
+
+
 ## Author
 
 👤 **Alex Castillo**
--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+/* Create the Database */
+CREATE DATABASE vet_clinic;
+
+/* Create table animals */
+CREATE TABLE IF NOT EXISTS animals (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(250) NOT NULL,
+	date_of_birth DATE NOT NULL DEFAULT now(),
+	escape_attempts INT,
+	neutered BOOLEAN DEFAULT false,
+	weight_kg NUMERIC,
+	PRIMARY KEY(id)
 );
